eef0aa1ceffee9a4eb2ca9de329f7037a0b85115
Caleb <caleb@theparker.me>
2017-08-03 14:41:36 -0700

    adding top container

--- src/components/TopCont.js
+++ src/components/TopCont.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
-import '../assets/styles/css/cTopCont.css';
+import React, { Component } from 'react'
+import '../assets/styles/css/cTopCont.css'
+import SearchCont from './TopCont/SearchCont'
 
 class TopCont extends Component {
   render() {
     return (
       <div className="cTopCont">
-        
+        <SearchCont />
       </div>
     )
   }
---
+++ src/components/TopCont/SearchCont.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import '../../assets/styles/css/cSearchCont.css';
+
+class SearchCont extends Component {
+  render() {
+    return (
+      <div className="cSearchCont">
+        <div className="cInput">
+          <div>
+          <input className="cInpText" type="text" placeholder="Search..." />
+          </div>
+          
+        </div>
+        <div className="cTopData">
+
+        </div>
+            
+          
+        </div>
+    )
+  }
+}
+
+export default SearchCont;
